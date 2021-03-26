552e5a77e265594cfad0cce26268668257fe6e89
Ashiqur Rahman <ashikrobi@gmail.com>
2021-03-26 14:04:12 +0600

    added name in book component

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAxKLqe0aJAEBIQWG-v4rlQyM_ZoIRsxAc",
+  authDomain: "burj-al-arab-7afbd.firebaseapp.com",
+  projectId: "burj-al-arab-7afbd",
+  storageBucket: "burj-al-arab-7afbd.appspot.com",
+  messagingSenderId: "829063774930",
+  appId: "1:829063774930:web:01bd9853322481ee18630b"
+};
+  export default firebaseConfig;
